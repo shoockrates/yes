fb99765c91063f683a40ca4613b11c62b1ee8a13
shoockrates <146773354+shoockrates@users.noreply.github.com>
2025-12-04 18:08:07 +0300

    Create clean.sql

---
+++ clean.sql
@@ -0,0 +1,18 @@
+-- ============================================================
+-- DB VALYMO SKRIPTAS
+-- ============================================================
+
+\echo ''
+\echo '======================================'
+\echo 'TRINAMI VISI kisp0844 OBJEKTAI...'
+\echo '======================================'
+
+-- Visa schema su viskuo
+DROP SCHEMA IF EXISTS kisp0844 CASCADE;
+
+\echo ''
+\echo 'IŠVALYTA SĖKMINGAI!'
+\echo ''
+
+-- Patikrink ar liko kas nors
+\dn
